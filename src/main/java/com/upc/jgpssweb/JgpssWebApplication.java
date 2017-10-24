6ee457b8ef20eb28dcba059e4dc8d3fc27ea1746
Ezequiel Andujar <eZe@MacBook-Pro-2.local>
2017-10-24 22:33:00 +0200

    Import Project

---
+++ src/main/java/com/upc/jgpssweb/JgpssWebApplication.java
@@ -0,0 +1,12 @@
+package com.upc.jgpssweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgpssWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgpssWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/upc/jgpssweb/JgpssWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upc.jgpssweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JgpssWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
